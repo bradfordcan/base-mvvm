80ce63da4e324dc2884b192bfcff77991a3ddc06
bradfordcan <64176700+bradfordcan@users.noreply.github.com>
2020-12-08 12:47:54 +0800

    Add dependency for preferences

---
+++ app/src/main/java/com/android/basemvvm/glide/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.android.basemvvm.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule : AppGlideModule()
